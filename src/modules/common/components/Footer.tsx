9026c33ad3e8bf3aae6e216b65d4ab5ff76679e1
rn-santos227 <rn_santos227@yahoo.com>
2025-11-11 23:23:12 +0800

    created footer component file

---
+++ src/modules/common/components/Footer.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from 'react'
+const currentYear = new Date().getFullYear()
+interface FooterProps {
+  children?: ReactNode
+}
+
+export default function Footer({ children }: FooterProps) {
+  return (
+
+  }
+}
