f7e5814298f9d352e8180747a763a3ff5fbc6a2c
rn-santos227 <rn_santos227@yahoo.com>
2025-11-12 19:45:31 +0800

    added interface for text field component file

--- src/components/ui/TextField.tsx
+++ src/components/ui/TextField.tsx
@@ -3,3 +3,10 @@ import type { InputHTMLAttributes } from 'react'
 
 import { classNames } from '@/helpers/classNames'
 
+export interface TextFieldProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size'> {
+  label?: string
+  helperText?: string
+  error?: string
+  containerClassName?: string
+}
