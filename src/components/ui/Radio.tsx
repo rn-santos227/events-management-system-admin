3d028889fbe7047dadd62daab32e8ebdc33937b2
rn-santos227 <rn_santos227@yahoo.com>
2025-11-13 00:31:21 +0800

    created radio component file

---
+++ src/components/ui/Radio.tsx
@@ -0,0 +1,10 @@
+import { createContext, forwardRef, useContext, useId } from 'react'
+import type {
+  FieldsetHTMLAttributes,
+  InputHTMLAttributes,
+  ReactNode,
+} from 'react'
+
+import { classNames } from '@/helpers/classNames'
+
+type Orientation = 'vertical' | 'horizontal'
