816c418dccf4e3d78e42c84e2ac288b61814fedd
rn-santos227 <rn_santos227@yahoo.com>
2025-11-12 19:30:58 +0800

    created button component file

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,9 @@
+import { forwardRef } from 'react'
+import type { ButtonHTMLAttributes, ReactNode } from 'react'
+
+import { classNames } from '@/helpers/classNames'
+
+type ButtonVariant = 'primary' | 'secondary' | 'outline' | 'ghost'
+type ButtonSize = 'sm' | 'md' | 'lg'
+
+
