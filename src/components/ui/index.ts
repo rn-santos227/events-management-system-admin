6d6d1dc04dabd0daf3860430fa216409852a8dd8
rn-santos227 <rn_santos227@yahoo.com>
2025-11-13 00:03:48 +0800

    created index file forf ui components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,22 @@
+export { Button } from './Button'
+export type { ButtonProps } from './Button'
+export { TextField } from './TextField'
+export type { TextFieldProps } from './TextField'
+export {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} 
+from './Card'
+export type {
+  CardProps,
+  CardHeaderProps,
+  CardTitleProps,
+  CardDescriptionProps,
+  CardContentProps,
+  CardFooterProps,
+}
+from './Card'
