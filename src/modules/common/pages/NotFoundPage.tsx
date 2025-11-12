faf226987abb07a22a3d171b3c6dba01d14b594a
rn-santos227 <rn_santos227@yahoo.com>
2025-11-12 18:31:30 +0800

    updated footer and not found page files

--- src/modules/common/pages/NotFoundPage.tsx
+++ src/modules/common/pages/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion'
-import Footer from '@/modules/common/components/Footer'
+import Footer from '@/components/shared/Footer'
 
 export default function NotFoundPage() {
   return (
