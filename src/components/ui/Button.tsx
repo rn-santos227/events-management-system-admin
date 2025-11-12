0cf9e4cc516332b23cd2ad77fbe267c73560142e
rn-santos227 <rn_santos227@yahoo.com>
2025-11-12 19:31:16 +0800

    added interface in  button component file

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,4 +6,11 @@ import { classNames } from '@/helpers/classNames'
 type ButtonVariant = 'primary' | 'secondary' | 'outline' | 'ghost'
 type ButtonSize = 'sm' | 'md' | 'lg'
 
-
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant
+  size?: ButtonSize
+  leftIcon?: ReactNode
+  rightIcon?: ReactNode
+  isLoading?: boolean
+  loadingText?: string
+}
