e4bee937f5d8b46347bf58abf5853a269984c708
rn-santos227 <rn_santos227@yahoo.com>
2025-11-14 10:36:22 +0800

    updated api service file

---
+++ src/services/api.ts
@@ -0,0 +1,7 @@
+import axios, { type AxiosInstance, type AxiosRequestConfig, isAxiosError } from 'axios'
+
+import { API, type ApiGroup } from '../config/api'
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? '/'
+
+
