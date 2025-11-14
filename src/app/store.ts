d3dafdcbd42529c23e4132d6f88f54c12a9cc614
rn-santos227 <rn_santos227@yahoo.com>
2025-11-14 12:43:18 +0800

    added hooks and store files

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit'
+
+import { authReducer } from '@/modules/auth/store/AuthSlice'
+import { userReducer } from '@/modules/auth/store/UserSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    auth: authReducer,
+    user: userReducer,
+  },
   devTools: import.meta.env.DEV,
-});
+})
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
