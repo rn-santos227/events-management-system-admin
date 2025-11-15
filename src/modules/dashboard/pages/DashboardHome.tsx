9b3db8c53397bc3d99602093e0c1a9efd2bb7b51
rn-santos227 <rn_santos227@yahoo.com>
2025-11-15 12:26:48 +0800

    created dashboard home page file

---
+++ src/modules/dashboard/pages/DashboardHome.tsx
@@ -0,0 +1,6 @@
+import { useMemo } from 'react'
+
+import { useAppSelector } from '@/app/hooks'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui'
+import { getUserFullName } from '@/types/user'
+
