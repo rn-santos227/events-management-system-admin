b644d7f6484007b1574d86ec932381baa88a88ba
rn-santos227 <rn_santos227@yahoo.com>
2025-11-15 12:51:55 +0800

    created api and routes constant files

---
+++ src/constants/api.ts
@@ -0,0 +1,6 @@
+export const API_ENDPOINTS = {
+  AUTH: {
+    ROOT: '/api/v1/auth',
+    LOGIN: '/api/v1/auth/login',
+  },
+} as const
---
+++ src/constants/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTES = {
+  LOGIN: '/',
+  DASHBOARD: '/dashboard',
+  NOT_FOUND: '*',
+} as const
