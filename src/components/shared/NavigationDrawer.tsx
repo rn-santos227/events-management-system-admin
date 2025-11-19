d7de6c4e3226967b7e6f6cb3a6457f9f4c24b5be
rn-santos227 <rn_santos227@yahoo.com>
2025-11-19 21:56:00 +0800

    updated navigation drawer component file

--- src/components/shared/NavigationDrawer.tsx
+++ src/components/shared/NavigationDrawer.tsx
@@ -1,16 +1,20 @@
 import { NavLink } from 'react-router-dom'
 
 import { DrawerLink } from '@/types/drawer'
+
 import { 
   getUserFullName,
   type UserProfile
 } from '@/types/user'
+import {
+  HiOutlineArrowsRightLeft,
+} from 'react-icons/hi2'
 
 const statusCopy: Record<NonNullable<DrawerLink['status']>, string> = {
   beta: 'Beta',
   soon: 'Coming soon',
 }
 
-const navigationSections: DrawerSection[] = [
+export default function NavigationDrawer({ userProfile }: NavigationDrawerProps) {
 
-]
+}
