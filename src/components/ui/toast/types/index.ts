144978eeff52b08bb5c5cc23a68598712fb6b689
rn-santos227 <rn_santos227@yahoo.com>
2025-11-19 22:32:12 +0800

    updated toast context file

--- src/components/ui/toast/types/index.ts
+++ src/components/ui/toast/types/index.ts
@@ -1,1 +1,21 @@
 export type ToastType = 'success' | 'info' | 'warning' | 'error'
+
+
+export interface ToastOptions {
+  title?: string
+  message: string
+  type?: ToastType
+  duration?: number
+}
+
+export interface ToastItem extends Required<Pick<ToastOptions, 'message'>> {
+  id: number
+  title?: string
+  type: ToastType
+  duration: number
+}
+
+export interface ToastContextValue {
+  showToast: (options: ToastOptions) => number
+  hideToast: (id: number) => void
+}
