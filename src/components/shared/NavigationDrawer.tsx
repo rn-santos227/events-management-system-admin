667894d75f55f3c2f79ac6d38dfeb4618e88a8a2
rn-santos227 <rn_santos227@yahoo.com>
2025-11-19 20:31:43 +0800

    created navigation drawer file

---
+++ src/components/shared/NavigationDrawer.tsx
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router-dom'
+
+import { ROUTES } from '@/constants/routes'
+import { DrawerLink, DrawerSection } from '@/types/drawer'
+import { 
+  getUserFullName,
+  type UserProfile
+} from '@/types/user'
+
+
