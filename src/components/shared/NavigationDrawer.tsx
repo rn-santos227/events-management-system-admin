56d22c4e5ba597d0890005845c648a9e26a918e5
rn-santos227 <rn_santos227@yahoo.com>
2025-11-19 20:36:51 +0800

    added status copy in navigation drawer component file

--- src/components/shared/NavigationDrawer.tsx
+++ src/components/shared/NavigationDrawer.tsx
@@ -1,7 +1,13 @@
 import { NavLink } from 'react-router-dom'
 
-import { ROUTES } from '@/constants/routes'
+import { DrawerLink } from '@/types/drawer'
 import { 
   getUserFullName,
   type UserProfile
 } from '@/types/user'
+
+const statusCopy: Record<NonNullable<DrawerLink['status']>, string> = {
+  beta: 'Beta',
+  soon: 'Coming soon',
+}
+
