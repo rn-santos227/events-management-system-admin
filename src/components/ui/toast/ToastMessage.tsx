366980855e88cb0788bbaaa59e5fdb18ba2fb671
rn-santos227 <rn_santos227@yahoo.com>
2025-11-20 20:52:35 +0800

    created toast message component file

---
+++ src/components/ui/toast/ToastMessage.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from 'react'
+
+import {
+  HiMiniCheckCircle,
+  HiMiniExclamationCircle,
+  HiMiniExclamationTriangle,
+  HiMiniInformationCircle,
+  HiMiniXMark,
+} from 'react-icons/hi2'
+
+import type { ToastItem } from './types'
+import { classNames } from '@/utils'
+
+
+
