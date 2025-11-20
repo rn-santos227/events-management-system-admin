307d1ca3dd15ece4f5f1aba51e4e26311d4646b1
rn-santos227 <rn_santos227@yahoo.com>
2025-11-20 21:02:51 +0800

    created toast provider component file

---
+++ src/components/ui/toast/ToastProvider.tsx
@@ -0,0 +1,9 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import type { ReactNode } from 'react'
+
+import { ToastContext } from './context'
+import { ToastRenderer } from './ToastRenderer'
+import type { ToastItem, ToastOptions } from './types'
+
+
+
