da09ecba0993d2fe1c8877b5ee0c96bfb7e7a90f
rn-santos227 <rn_santos227@yahoo.com>
2025-11-20 20:55:44 +0800

    updated toast config component file

--- src/components/ui/toast/ToastConfig.tsx
+++ src/components/ui/toast/ToastConfig.tsx
@@ -15,9 +15,12 @@ export const typeStyles: Record<ToastItem['type'], { icon: ReactNode; accentClas
     icon: <HiMiniCheckCircle className="h-5 w-5 text-green-500" aria-hidden="true" />,
     accentClass: 'border-green-200',
   },
-
   info: {
     icon: <HiMiniInformationCircle className="h-5 w-5 text-blue-500" aria-hidden="true" />,
     accentClass: 'border-blue-200',
+  },
+  warning: {
+    icon: <HiMiniExclamationTriangle className="h-5 w-5 text-amber-500" aria-hidden="true" />,
+    accentClass: 'border-amber-200',
   },
 }
