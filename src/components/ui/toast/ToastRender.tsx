066a5ff6e2d849f6717885f4c75f84ca310945c9
rn-santos227 <rn_santos227@yahoo.com>
2025-11-20 21:00:52 +0800

    updated toast renderer component file

--- src/components/ui/toast/ToastRender.tsx
+++ src/components/ui/toast/ToastRender.tsx
@@ -1,2 +1,9 @@
 import type { ToastItem } from './types'
 import { ToastMessage } from './ToastMessage'
+
+interface ToastRendererProps {
+  toasts: ToastItem[]
+  onDismiss: (id: number) => void
+}
+
+
