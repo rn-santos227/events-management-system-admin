b6fc96cd0e48328d46e0880ef8df1a2b367a0db5
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 19:36:09 +0800

    updated privileges constant file

--- src/constants/privileges.ts
+++ src/constants/privileges.ts
@@ -1,7 +1,12 @@
 export const PRIVILEGE_ACTIONS = {
-
+  PRIVILEGES: {
+    CREATE: 'privileges:create',
+    READ: 'privileges:read',
+    UPDATE: 'privileges:update',
+    DELETE: 'privileges:delete',
+  },
 } as const
 
 export type PrivilegeAction =
   (typeof PRIVILEGE_ACTIONS)[keyof typeof PRIVILEGE_ACTIONS][keyof (typeof PRIVILEGE_ACTIONS)[keyof typeof PRIVILEGE_ACTIONS]]
-
+  
