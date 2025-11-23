9fd1827edcf5816175b25fee78b7dc5a3cff1f1d
rn-santos227 <rn_santos227@yahoo.com>
2025-11-23 19:37:36 +0800

    updated privileges constant file

--- src/constants/privileges.ts
+++ src/constants/privileges.ts
@@ -1,16 +1,36 @@
 export const PRIVILEGE_ACTIONS = {
+  AUDIT_LOGS: {
+    READ: 'audit_logs:read',
+    READ_OWN: 'audit_logs:read:own',
+  },
+
+  FILES: {
+    READ: 'files:read',
+    DOWNLOAD: 'files:download',
+    UPLOAD: 'files:upload',
+    DELETE: 'files:delete',
+  },
+
   PRIVILEGES: {
     CREATE: 'privileges:create',
     READ: 'privileges:read',
     UPDATE: 'privileges:update',
     DELETE: 'privileges:delete',
   },
+
   ROLES: {
     CREATE: 'roles:create',
     READ: 'roles:read',
     UPDATE: 'roles:update',
     DELETE: 'roles:delete',
   },
+
+  USERS: {
+    CREATE: 'users:create',
+    READ: 'users:read',
+    UPDATE: 'users:update',
+    DELETE: 'users:delete',
+  }
 } as const
 
 export type PrivilegeAction =
