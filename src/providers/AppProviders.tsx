124af1512f40db70e5d2a5c5ea54d3472a65238b
rn-santos227 <rn_santos227@yahoo.com>
2025-11-25 16:48:41 +0800

    updated app provider file

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 
 import { store } from '../app/store'
+import { GraphQLProvider } from './GraphQLProvider'
 import { DialogProvider, ToastProvider } from '@/components/ui'
 
 interface AppProvidersProps {
@@ -11,9 +12,11 @@ interface AppProvidersProps {
 export function AppProviders({ children }: AppProvidersProps) {
   return (
     <Provider store={store}>
-      <ToastProvider>
-        <DialogProvider>{children}</DialogProvider>
-      </ToastProvider>
+      <GraphQLProvider>
+        <ToastProvider>
+          <DialogProvider>{children}</DialogProvider>
+        </ToastProvider>
+      </GraphQLProvider>
     </Provider>
   )
 }
