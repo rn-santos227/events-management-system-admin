35fd6b962e3bd66d51d3ecae2b36cf5cd6405c72
rn-santos227 <rn_santos227@yahoo.com>
2025-11-25 15:16:11 +0800

    updated css index file for text field comopnent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import colors from 'tailwindcss/colors'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,18 +9,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        brands: {
-          50: '#eff6ff',
-          100: '#dbeafe',
-          200: '#bfdbfe',
-          300: '#93c5fd',
-          400: '#60a5fa',
-          500: '#3b82f6',
-          600: '#2563eb',
-          700: '#1d4ed8',
-          800: '#1e40af',
-          900: '#1e3a8a',
-        },
+        brand: colors.blue,
       },
       boxShadow: {
         card: '0 6px 20px rgba(0, 0, 0, 0.08)'
