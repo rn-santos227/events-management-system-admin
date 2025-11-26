e0ccd9a13d4a2b55b2c05fada13a8092d08753cc
rn-santos227 <rn_santos227@yahoo.com>
2025-11-26 20:14:53 +0800

    created alert banner ui component file

---
+++ src/components/ui/alert/AlertBanner.tsx
@@ -0,0 +1,7 @@
+import type { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { HiCheckCircle, HiExclamationTriangle, HiInformationCircle, HiXCircle, HiXMark } from 'react-icons/hi2'
+import type { IconType } from 'react-icons'
+
+import './index.css'
+import { classNames } from '@/utils/classNames'
+
