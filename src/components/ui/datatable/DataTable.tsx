a8e29a6d107d679ebad8331ec26bc71e9bf75d8f
rn-santos227 <rn_santos227@yahoo.com>
2025-11-26 19:25:44 +0800

    created data table component file

---
+++ src/components/ui/datatable/DataTable.tsx
@@ -0,0 +1,8 @@
+import './index.css'
+import { classNames } from '@/utils/classNames'
+
+export type DataTableAlign = 'left' | 'center' | 'right'
+export type DataTableSortDirection = 'asc' | 'desc'
+
+
+
