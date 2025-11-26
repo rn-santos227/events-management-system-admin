191e42442d5b6f5ce2b75afaf5c2037d1260e2e7
rn-santos227 <rn_santos227@yahoo.com>
2025-11-26 19:26:14 +0800

    updated data table component file

--- src/components/ui/datatable/DataTable.tsx
+++ src/components/ui/datatable/DataTable.tsx
@@ -1,8 +1,20 @@
+import { useMemo, useState } from 'react'
+import type { ReactNode } from 'react'
+
 import './index.css'
 import { classNames } from '@/utils/classNames'
 
 export type DataTableAlign = 'left' | 'center' | 'right'
 export type DataTableSortDirection = 'asc' | 'desc'
 
-
+export interface DataTableColumn<T> {
+  key: keyof T | string
+  header: string
+  width?: string
+  align?: DataTableAlign
+  render?: (row: T, index: number) => ReactNode
+  sortable?: boolean
+  sortAccessor?: (row: T) => string | number | Date | undefined | null
+  hideLabel?: boolean
+}
 
