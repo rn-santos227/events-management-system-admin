f4589bea3678ee21dc754124e01b6acd898c0996
rn-santos227 <rn_santos227@yahoo.com>
2025-11-27 23:00:59 +0800

    updated audit logs page file

--- src/modules/audit/pages/AuditLogs.tsx
+++ src/modules/audit/pages/AuditLogs.tsx
@@ -2,4 +2,20 @@ import { useMemo, useState } from 'react'
 import { useQuery } from '@apollo/client'
 
 import { useAppSelector } from '@/app/hooks'
-
+import {
+  AlertBanner,
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  DataTable,
+  type DataTableColumn,
+  TextField,
+} from '@/components/ui'
+import { PRIVILEGE_ACTIONS } from '@/constants/privileges'
+import { useAuthorization } from '@/modules/auth/hooks/useAuthorization'
+import { AUDIT_LOGS_BY_USER_QUERY, AUDIT_LOGS_QUERY } from '../queries'
+import type { AuditLogEntry } from '@/types/auditLog'
+import { getAuditUserLabel } from '@/types/auditLog'
