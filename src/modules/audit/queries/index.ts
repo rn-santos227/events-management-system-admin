6e1c1a019a0bb20f2e841e38971eaa1561a01325
rn-santos227 <rn_santos227@yahoo.com>
2025-11-27 22:56:07 +0800

    updated audit query index file

--- src/modules/audit/queries/index.ts
+++ src/modules/audit/queries/index.ts
@@ -1,3 +1,19 @@
 import { gql } from '@apollo/client'
 
-
+export const AUDIT_LOG_FIELDS = gql`
+  fragment AuditLogFields on AuditLog {
+    id
+    action
+    method
+    path
+    statusCode
+    ipAddress
+    message
+    createdAt
+    user {
+      id
+      email
+      fullName
+    }
+  }
+`
