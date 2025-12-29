2aa7fad8bb9ec9c6be70d0cdf85ed9e019b68176
rn-santos227 <rn_santos227@yahoo.com>
2025-12-29 22:09:51 +0800

    updated graphql provider file

--- src/providers/GraphQLProvider.tsx
+++ src/providers/GraphQLProvider.tsx
@@ -2,7 +2,7 @@ import { ApolloProvider } from '@apollo/client'
 import type { ReactNode } from 'react'
 import { useMemo } from 'react'
 
-import { useAppSelector } from '../app/hooks'
+import { useAuthToken } from '@/hooks'
 import { createGraphQLClient } from '@/services/graphql'
 
 interface GraphQLProviderProps {
@@ -10,9 +10,7 @@ interface GraphQLProviderProps {
 }
 
 export function GraphQLProvider({ children }: GraphQLProviderProps) {
-  const authToken = useAppSelector((state) => state.auth.token)
-
+  const authToken = useAuthToken()
   const client = useMemo(() => createGraphQLClient(authToken), [authToken])
-
   return <ApolloProvider client={client}>{children}</ApolloProvider>
 }
