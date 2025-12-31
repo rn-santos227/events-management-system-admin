3e04f6914414bdd5d2ce7286b27cffd98b37a6ca
rn-santos227 <rn_santos227@yahoo.com>
2025-12-31 21:22:45 +0800

    updated api constant file

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,11 @@
 export const BASE_API_PREFIX = "/api/v1/"
 export const API_ENDPOINTS = {
+  AUDIT_LOGS: {
+    ROOT: 'audit-logs',
+    SEARCH: 'audit-logs/search',
+    BY_USER: 'audit-logs/:id',
+  },
+ 
   AUTH: {
     ROOT: 'auth',
     LOGIN: 'auth/login',
