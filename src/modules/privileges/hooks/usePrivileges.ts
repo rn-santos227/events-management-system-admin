a0b50e1d18322310633601998a641f17a02027a0
rn-santos227 <rn_santos227@yahoo.com>
2026-01-02 22:29:39 +0800

    updated privielges hook file

--- src/modules/privileges/hooks/usePrivileges.ts
+++ src/modules/privileges/hooks/usePrivileges.ts
@@ -5,5 +5,11 @@ import { fetchPrivileges, updatePrivilegeStatus } from '../store/privilegesSlice
 import type { Privilege } from '../types/privilege'
 
 export const usePrivileges = () => {
+  const dispatch = useAppDispatch()
+  const items = useAppSelector((state) => state.privileges.items)
+  const status = useAppSelector((state) => state.privileges.status)
+  const error = useAppSelector((state) => state.privileges.error)
+  const updatingIds = useAppSelector((state) => state.privileges.updatingIds)
+  const updateError = useAppSelector((state) => state.privileges.updateError)
 
 }
