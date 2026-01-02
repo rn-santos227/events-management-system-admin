d7948a35cc2089e8ed59020a8aab221572161174
rn-santos227 <rn_santos227@yahoo.com>
2026-01-02 22:17:49 +0800

    created privilege slice file

--- src/modules/privileges/store/PrivilegesSlice.ts
+++ src/modules/privileges/store/PrivilegesSlice.ts
@@ -3,4 +3,11 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { apiClient, type ApiErrorPayload } from '@/services/api'
 import type { Privilege } from '../types/privilege'
 
+interface FetchPrivilegesArgs {
+  limit?: number
+}
 
+interface UpdatePrivilegeStatusArgs {
+  privilege: Privilege
+  active: boolean
+}
