c4173f21349b41184666995fcba7583703983821
rn-santos227 <rn_santos227@yahoo.com>
2026-01-03 22:13:39 +0800

    updated privilege page file in privileges

---
+++ src/modules/privileges/pages/PrivilegesPage.tsx
@@ -0,0 +1,19 @@
+import { useCallback, useEffect } from 'react'
+
+import {
+  AlertBanner,
+  Button,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  DataTable,
+} from '@/components/ui'
+import { PRIVILEGE_ACTIONS } from '@/constants/privileges'
+import { useAuthorization } from '@/modules/auth/hooks/useAuthorization'
+import { usePrivileges } from '../hooks/usePrivileges'
+import { usePrivilegeColumns } from '../hooks/usePrivilegeColumns'
+import type { Privilege } from '../types/privilege'
+import { isPrivilegeActive } from '../types/privilege'
+
