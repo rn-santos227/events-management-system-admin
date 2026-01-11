4b9946c201942bd252e55c5587899e2d8f5d90ee
rn-santos227 <rn_santos227@yahoo.com>
2026-01-11 21:45:23 +0800

    created user setting provider file

---
+++ src/providers/UserSettingsProvider.tsx
@@ -0,0 +1,11 @@
+import type { ReactNode } from 'react'
+import { useEffect } from 'react'
+
+import {
+  applyUserSettings,
+  loadUserSettings,
+  normalizeUserSettings,
+  subscribeToSystemTheme,
+} from '@/modules/profile/utils/userSettings'
+
+
